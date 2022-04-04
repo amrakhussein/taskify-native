7972a35f3e654285da2a5d4ab663699111e857e5
Amro Abdelkamel <amromoorie@gmail.com>
2022-04-04 23:41:07 +0200

    Refactor(app structure) + basic components & data setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { Platform, StyleSheet, Text, View } from 'react-native'
+import { Button } from 'react-native-web'
+import TaskListItem from './AppTask/TaskListItem'
 // import { SafeAreaView } from 'react-native-web'
-import TaskListItem from './components/AppTask/TaskListItem'
+// import colors from './styles/colors'
+import tasks from '../tasksData'
+console.log('tasks: ', tasks);
+
 
 export default function App() {
+
+  //  Alert.alert("message", 'message', [{text: "yes", onPress: () => }, {text:'no'}])
+  // Alert.promt("title", "mss", text => console.log(text))
   return (
     <View style={styles.container}>
+      <StatusBar style='auto' />
       {/* taskify header */}
       <View style={styles.appBar}>
         <View style={styles.appBarItems}>
@@ -16,10 +25,16 @@ export default function App() {
       </View>
 
       <View style={styles.taskList}>
-        <TaskListItem />
+        {
+          tasks.map((item, idx) => (
+            
+            <TaskListItem key={idx} task={item} />
+          ))
+        }
       </View>
 
-      <StatusBar style='auto' />
+
+<Button title='' color=''></Button>
      </View>
   )
 }
@@ -28,18 +43,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#eee',
-    paddingTop: 40 
+    // paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
 
   },
   appBarBrand:{
 
     fontSize: 25,
     fontWeight: 'bold',
+    paddingLeft: 10,
+    fontStyle:'italic',
   },
 
   appBar: {
     backgroundColor:'lightblue',
     height: 70,
+    // elevation: 50,
   },
   taskList: {
     flex: 1,
@@ -50,5 +68,6 @@ const styles = StyleSheet.create({
     flex:1,
     justifyContent:'space-around',
     alignContent:'center',
+    backgroundColor:'lightblue'
   }
 })
--- src/components/AppTask/TaskListItem.jsx
+++ src/components/AppTask/TaskListItem.jsx
@@ -1,16 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 
 
-export default function TaskListItem() {
+export default function TaskListItem({task}) {
   return (
     <View style={styles.listItemContainer}>
       <View style={styles.listItem}>
         {/* <Touch */}
         <Text numberOfLines={3}>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-          animi omnis quasi saepe voluptas facere, eius maiores veniam deserunt
-          sapiente placeat quidem incidunt illo voluptates laudantium fugiat ad.
-          Ducimus, maxime.fsfsfsfsf
+          {task.content}
         </Text>
       </View>
     </View>
@@ -21,6 +18,12 @@ const styles = StyleSheet.create({
   listItemContainer: {
   },
   listItem: {
+    marginVertical: 10,
+    backgroundColor: 'cyan',
+    paddingHorizontal: 5,
+    paddingVertical:4,
+    borderRadius:5
+
   },
 
 
